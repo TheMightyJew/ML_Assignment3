7daf715dedb6cf0a2dfb199ddfabc0fa504814dd
Rotem-Lev-Lehman <levlerot@post.bgu.ac.il>
2020-07-04 14:25:06 +0300

    stuff

--- organize_flowers.py
+++ organize_flowers.py
@@ -2,7 +2,7 @@ import os
 import scipy.io
 from shutil import copyfile
 
-test_size = 0.3
+test_size = 0.2
 
 image_labels_file = scipy.io.loadmat('data/imagelabels.mat')
 labels = list(image_labels_file['labels'][0])
@@ -11,21 +11,23 @@ for label in set(labels):
     dict[label] = set()
 for index, filename in enumerate(os.listdir('data/flowers_photos')):
     dict[labels[index]].add(filename)
+
+base_folder_name = 'organized_flowers_photos_' + str(test_size)
 try:
-    os.mkdir('data/organized_flowers_photos')
-    os.mkdir('data/organized_flowers_photos/train')
-    os.mkdir('data/organized_flowers_photos/test')
+    os.mkdir('data/' + base_folder_name)
+    os.mkdir('data/' + base_folder_name + '/train')
+    os.mkdir('data/' + base_folder_name + '/test')
 except:
     print("error")
 for label in dict.keys():
     try:
-        os.mkdir('data/organized_flowers_photos/train/' + str(label))
-        os.mkdir('data/organized_flowers_photos/test/' + str(label))
+        os.mkdir('data/' + base_folder_name + '/train/' + str(label))
+        os.mkdir('data/' + base_folder_name + '/test/' + str(label))
     except:
         print("error")
     length = len(dict[label]) - 1
     for i, filename in enumerate(dict[label]):
         if i <= test_size * length:
-            copyfile('data/flowers_photos/' + filename, 'data/organized_flowers_photos/test/' + str(label) + '/' + filename)
+            copyfile('data/flowers_photos/' + filename, 'data/' + base_folder_name + '/test/' + str(label) + '/' + filename)
         else:
-            copyfile('data/flowers_photos/' + filename, 'data/organized_flowers_photos/train/' + str(label) + '/' + filename)
+            copyfile('data/flowers_photos/' + filename, 'data/' + base_folder_name + '/train/' + str(label) + '/' + filename)
